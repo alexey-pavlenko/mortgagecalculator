bd8e340fb766c1779c743e763ee28ecf89fd0241
prtnsu <protolicious@hotmail.com>
2022-08-05 03:48:51 +0400

    Create Mortgagev3.java

---
+++ Mortgagev3.java
@@ -0,0 +1,20 @@
+package com.gethigh;
+import java.text.NumberFormat;
+
+public class Mortgagev3 {
+    public static void main(String[] args) {
+
+        var mort = new MinAndMaxForMortgagev3();
+        mort.valuePrincipalMinMax(25_000,90_000);
+
+       mort.valuePeriodMinMax((byte) 1, (byte) 29);
+
+       mort.annualRateValueMinMax((float) 0.1, (float) 20.1);
+
+        float mortgageResult = mort.result();
+
+        String resultDollars = NumberFormat.getCurrencyInstance().format(mortgageResult);
+
+        System.out.println("Your monthly payment: " + resultDollars);
+    }
+}
