82eb114be108b6ac0a86919056b23d977f64a42f
Alexey <protolicious@hotmail.com>
2023-10-18 01:24:43 +0300

    Remove redundant variables

--- src/com/gethigh/MortgageV3.java
+++ src/com/gethigh/MortgageV3.java
@@ -5,11 +5,11 @@ import java.text.NumberFormat;
 public class MortgageV3 {
     public static void main(String[] args) {
 
-        var mortPrincipal = new MortgagePrincipal(25_000,90_000);
+        new MortgagePrincipal(25_000,90_000);
 
-        var mortValuePeriod = new MortgageValuePeriod((byte) 1, (byte) 29);
+        new MortgageValuePeriod((byte) 1, (byte) 29);
 
-        var mortValueAnnualRate = new MortgageValueAnnualRate((float) 0.1, (float) 20.1);
+        new MortgageValueAnnualRate((float) 0.1, (float) 20.1);
 
         var finalMortgageCalculations = new FinalMortgageCalculations();
 
